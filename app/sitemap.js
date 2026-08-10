85a47ae2f4b75563c3ceb1cfe2ad71b08b4f1ff9
vineet-proptiger <vineet.mishra@proptiger.com>
2026-08-10 15:29:01 +0530

    som changes in sitmap

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,18 +6,18 @@ export default function sitemap() {
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
-    },
-    {
-      url: `${baseUrl}/new-launch-bangalore`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.3,
-    },
+    }
+    // {
+    //   url: `${baseUrl}/new-launch-bangalore`,
+    //   lastModified: new Date(),
+    //   changeFrequency: 'weekly',
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: `${baseUrl}/privacy-policy`,
+    //   lastModified: new Date(),
+    //   changeFrequency: 'yearly',
+    //   priority: 0.3,
+    // },
   ]
 }
